6405836864040e7c85003ace0a5fc59b3e49eaa0
nummer14 <nummer14@naver.com>
2025-08-06 09:36:48 +0900

    lock

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,31 @@
 public class Main {
+    private static final Object lockA = new Object();
+    private static final Object lockB = new Object();
+
     public static void main(String[] args) {
-        PrintQueue queue = new PrintQueue();
+        Thread t1 = new Thread(() -> {
+            synchronized (lockA) {
+                System.out.println("Thread-1: lockA 획득");
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+                System.out.println("Thread-1: lockB 대기 중...");
+                synchronized (lockB) {
+                    System.out.println("Thread-1: lockB 획득");
+                }
+            }
+        });
 
-        User[] users = {
-                new User(queue, "사용자 1"),
-                new User(queue, "사용자 2"),
-                new User(queue, "사용자 3"),
-                new User(queue, "사용자 4"),
-                new User(queue, "사용자 5"),
-        };
+        Thread t2 = new Thread(() -> {
+            synchronized (lockB) {
+                System.out.println("Thread-2: lockB 획득");
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+                System.out.println("Thread-2: lockA 대기 중...");
+                synchronized (lockA) {
+                    System.out.println("Thread-2: lockA 획득");
+                }
+            }
+        });
 
-        for (User user: users) {
-            user.start();
-        }
+        t1.start();
+        t2.start();
     }
 }
